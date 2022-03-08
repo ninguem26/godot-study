11114567d15627f27b74b0fd47dd5309e51767ce
Júlio César <diolandajr@gmail.com>
2022-03-07 21:05:52 -0300

    feat: implementado o pulo do personagem

--- Hunter Garden/scripts/player/jungle_guy.gd
+++ Hunter Garden/scripts/player/jungle_guy.gd
@@ -1,15 +1,20 @@
 extends KinematicBody2D
 
 var velocity: Vector2
+var jump_count: int = 0
 
 export(int) var walk_speed
+export(int) var jump_speed
+export(int) var gravity
 
 export(float) var acceleration
 export(float) var friction
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	move()
-	velocity = move_and_slide(velocity)
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity, Vector2.UP)
+	jump()
 
 func move() -> void:
 	var input_vector: Vector2 = Vector2.ZERO
@@ -23,3 +28,11 @@ func move() -> void:
 		velocity.x = lerp(velocity.x, walk_speed * input_vector.x, acceleration)
 	else:
 		velocity.x = lerp(velocity.x, 0, friction)
+
+func jump() -> void:
+	if is_on_floor():
+		jump_count = 0
+	
+	if Input.is_action_just_pressed("Jump") and jump_count < 2:
+		jump_count += 1
+		velocity.y = -jump_speed
