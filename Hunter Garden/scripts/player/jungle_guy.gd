e7a98fb336276b96231fa09895bd0152168e9126
Júlio César <diolandajr@gmail.com>
2022-03-12 13:27:41 -0300

    change: implementando dash

--- Hunter Garden/scripts/player/jungle_guy.gd
+++ Hunter Garden/scripts/player/jungle_guy.gd
@@ -3,8 +3,12 @@ extends KinematicBody2D
 const SMOKEFX = preload("res://scenes/effects/smoke_fx.tscn")
 
 var velocity: Vector2
+var dash_direction: Vector2
 
 var jump_count: int = 0
+
+var can_dash: bool = false
+var is_dashing: bool = false
 
 export(int) var walk_speed
 export(int) var jump_speed
@@ -13,11 +17,14 @@ export(int) var idle_threshold
 
 export(float) var acceleration
 export(float) var friction
+export(float) var dash_length
 
 onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
+onready var dash_timer: Timer = get_node("DashTimer")
 
 func _physics_process(delta: float) -> void:
 	move()
+	handle_dash()
 	animate()
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -76,3 +83,17 @@ func spawn_dust_effect() -> void:
 	
 	get_tree().root.call_deferred("add_child", dust)
 	dust.global_position = global_position + Vector2(0, 1)
+
+func handle_dash() -> void:
+	if Input.is_action_just_pressed("Dash") and can_dash and not is_on_floor():
+		is_dashing = true
+		can_dash = false
+		dash_direction = dash()
+		dash_timer.start(dash_length)
+
+func dash() -> Vector2:
+	var input_vector: Vector2 = Vector2.ZERO
+	return Vector2.ZERO
+
+func on_dash_timeout() -> void:
+	is_dashing = false
