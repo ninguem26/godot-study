710abb90f90b470177f7bea31cdccdafbd8d9c1a
Júlio César <diolandajr@gmail.com>
2022-07-29 23:42:13 +0000

    change: adicionado código do projétil

--- Dyson/scripts/gun_shield.gd
+++ Dyson/scripts/gun_shield.gd
@@ -6,26 +6,27 @@ export(float) var speed = 30.0
 export(float) var acceleration = 1.0
 
 onready var pivot: StaticBody2D = get_node(pivot_node)
+onready var projectile_position: Position2D = get_node("ProjectilePosition")
 
 var velocity: Vector2
 var pivot_point: Vector2
 
-var current_angle: float = 0.0
+var current_angle: float = PI
 
 func _ready() -> void:
 	pivot_point = pivot.global_position
-	
-func _physics_process(delta: float) -> void:
+
+func _physics_process(_delta: float) -> void:
 	move(input_dir())
 
-func rotated_point(angle, distance):
+func rotated_point(angle, distance) -> Vector2:
 	return pivot_point + Vector2(sin(angle),cos(angle)) * distance
 
 func move(direction: int) -> void:
 	var distance: float = pivot_point.distance_to(global_position)
 	var angular_speed: float = speed/distance
 	
-	current_angle += angular_speed*direction
+	current_angle -= angular_speed*direction
 	position = rotated_point(current_angle, distance)
 	look_at(pivot_point)
 
--- Dyson/scripts/projectile.gd
+++ Dyson/scripts/projectile.gd
@@ -1,5 +1,9 @@
 extends Area2D
 
+export(float) var speed = 30.0
 
-func _ready() -> void:
-	pass
+func _physics_process(delta: float):
+	translate(Vector2(-speed*delta, 0))
+
+func on_screen_exited():
+	queue_free()
