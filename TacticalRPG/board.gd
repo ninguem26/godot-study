bdcc32bfe8c13a1e4bf828fa1039038084c036bd
Júlio César <ninguem26@users.noreply.github.com>
2023-02-08 20:57:40 -0300

    change: implementando highlight do board

--- TacticalRPG/board.gd
+++ TacticalRPG/board.gd
@@ -2,6 +2,7 @@ extends Node
 
 var cells
 var selected_cell
+var previous_cell
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,6 +18,7 @@ func _input(event) -> void:
 		var target_cell = ((event.position / 64).floor() * 64) + Vector2(32, 32)
 		selected_cell = find_cell_by_position(target_cell)
 		selected_cell.mark()
+		show_cell_radius(selected_cell, 1)
 
 func find_cell_by_position(position) -> Object:
 	for cell in cells:
@@ -26,5 +28,9 @@ func find_cell_by_position(position) -> Object:
 	
 	return null
 
-func show_radius(radius) -> void:
-	pass
+func show_cell_radius(cell, radius) -> void:
+	if radius >= 1:
+		for connection_cell in cell.conections:
+			if !connection_cell.selected:
+				connection_cell.mark()
+				show_cell_radius(connection_cell, radius - 1)
--- TacticalRPG/cell.gd
+++ TacticalRPG/cell.gd
@@ -3,13 +3,17 @@ extends Node2D
 export(Array, NodePath) var connections
 
 onready var sprite = get_node('Sprite')
+var selected = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func mark() -> void:
-	sprite.modulate = Color(0, 1, 0)
+	if !selected:
+		sprite.modulate = Color(0, 1, 0)
+		selected = true
 
 func unmark() -> void:
-	sprite.self_modulate = Color(255, 255, 255)
+	sprite.modulate = Color(1, 1, 1)
+	selected = false
