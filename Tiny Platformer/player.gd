07d546063ebc7075385cea063c0e2b46de2f277c
Júlio César <diolandajr@gmail.com>
2023-04-02 23:39:05 -0300

    change: adicionados itens coletáveis

--- Tiny Platformer/player.gd
+++ Tiny Platformer/player.gd
@@ -1,10 +1,16 @@
 extends CharacterBody2D
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var current_direction = 1
+
+@onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("idle")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -19,8 +25,14 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
 	if direction:
+		if direction != 0 and direction != current_direction:
+			current_direction = direction
+			scale.x *= -1
+		
+		animation_player.play("run")
 		velocity.x = direction * SPEED
 	else:
+		animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
---
+++ Tiny Platformer/star.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var animation_player = $AnimationPlayer
+@onready var sprite = $Sprite
+
+var TW
+var TW2
+
+func _ready():
+	TW = create_tween().set_loops()
+	TW2 = create_tween().set_loops().set_trans(Tween.TRANS_SINE).set_ease(Tween.EASE_IN_OUT)
+	
+	TW.tween_property(
+		sprite, 'scale', Vector2(1.2, 1.2), .5
+	).from_current().set_trans(Tween.TRANS_BOUNCE).set_ease(Tween.EASE_OUT)
+	TW.tween_property(
+		sprite, 'scale', Vector2(1, 1), .5
+	).set_trans(Tween.TRANS_CUBIC).set_ease(Tween.EASE_IN_OUT)
+	
+	TW2.tween_property(sprite, 'position', Vector2(0, 2.5), 1)
+	TW2.tween_property(sprite, 'position', Vector2(0, -1.5), 1)
+
+func on_body_entered(body):
+	# TW.finished()
+	# TW2.finished()
+	animation_player.play("collected")
