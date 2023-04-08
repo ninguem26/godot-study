b0d69c934f675e09af838f7a559c8aeb13e4abb4
Júlio César <diolandajr@gmail.com>
2023-04-08 00:56:56 -0300

    change: pequenas alterações

--- Tiny Platformer/player.gd
+++ Tiny Platformer/player.gd
@@ -31,7 +31,7 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction: int = int(Input.get_axis(move_left_key, move_right_key))
